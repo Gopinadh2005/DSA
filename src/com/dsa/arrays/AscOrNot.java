5664ed25e052eb02d476bde3f5252f28e666aabb
gopinadh <gopinadh@LAPTOP-JSG6M7F3>
2025-12-28 15:30:14 +0530

    Day1

---
+++ src/com/dsa/arrays/AscOrNot.java
@@ -0,0 +1,21 @@
+package com.dsa.arrays;
+
+public class AscOrNot {
+
+	public static void main(String[] args) {
+		int[] arr= {11,13,23,27,3};
+		
+		boolean isSorted=true;
+		for(int i=0;i<arr.length-1;i++) {
+			if(arr[i]>arr[i+1]) {
+				isSorted=false;
+				break;
+			}
+		}
+		if(isSorted)
+			System.out.println("Array is in Asc order");
+		else
+			System.out.println("Array is not in Asc order");
+	}
+
+}
